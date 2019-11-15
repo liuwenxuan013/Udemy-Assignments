a75ce1d75af327ff4ecd6eec07df1768aea75257
Wenxuan Liu <wliu122@myseneca.ca>
2019-11-15 04:05:22 -0500

    add char components

--- assignment2/src/App.js
+++ assignment2/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import Validation from './Validation/Validation'
+import Char from './Char/Char';
+
 import './App.css';
 
 class App extends React.Component
 {
   state = {
     userInput: '',
+
   }
   inputChangeHandler = (event) =>
   {
@@ -13,16 +16,38 @@ class App extends React.Component
       userInput: event.target.value
     })
   }
+  deleteCharHandler = (index) =>
+  {
+    const text = this.state.userInput.split('');
+    text.splice(index, 1);
+    const updatedText = text.join('');
+    this.setState({
+      userInput: updatedText
+    });
+
+  }
 
   render()
   {
+    const charList = this.state.userInput.split('').map((ch, index) =>
+    {
+      return <Char character={ch}
+        key={index}
+        onClick={() => this.deleteCharHandler(index)}
+
+      />
+    })
+
     return <div>
       <input type='text'
         onChange={this.inputChangeHandler}
+        value={this.state.userInput}
       />
-      <p>{this.state.userInput.length}</p>
+      <p>{this.state.userInput}</p>
 
       <Validation inputLength={this.state.userInput.length} />
+
+      {charList}
     </div>
   }
 
